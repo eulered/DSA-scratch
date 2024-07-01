3fd3ff2a0771048d89320f01f0af2db50e5e59bc
eulered <124056054+eulered@users.noreply.github.com>
2024-07-01 09:27:02 +0800

    added linkedlist class

--- linkedlist.py
+++ linkedlist.py
@@ -2,3 +2,22 @@ class Node:
   def __init__(self, value):
     self.value = value
     self.next = None
+
+
+class LinkedList:
+  def __init__(self, value):
+    new_node = Node(value)
+
+    self.head = new_node
+    self.tail = new_node
+    self.length = 1
+  
+  def print_val(self):
+    temp = self.head
+    while temp is not None:
+      print(temp.value)
+      temp = temp.next
+
+linkedlist1 = LinkedList(1)
+
+linkedlist1.print_val()
